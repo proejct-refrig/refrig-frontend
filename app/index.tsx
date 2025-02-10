4068392ce76c8ba5d6d750184afb670a1a088f97
Rowoon <dev.rowoon@gmail.com>
2025-02-10 15:10:00 +0900

    feat: 로그인 페이지 기본 틀 완성

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { Image } from 'expo-image'
 
 const AppLogo = require('@/assets/images/refrig_logo.png')
+const kakaoLogo = require('@/assets/images/logo_kakaotalk.png')
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.loginBox}>
+        {/* 로고 */}
         <Image source={AppLogo} style={styles.image}/>
+        {/* 로그인 제목 */}
+        <Text style={styles.mainText}>우리집 냉장고를 스마트하게 관리하세요!</Text>
+        {/* 소셜 로그인 버튼 */}
+        <View style={styles.buttonContainer}>
+          <Pressable style={styles.button}>
+            <Image source={kakaoLogo} style={styles.loginImage} />
+            <Text>카카오톡으로 로그인</Text>
+          </Pressable>
+        </View>
       </View>
     </View>
   );
@@ -32,10 +43,36 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+  mainText: {
+    fontSize: 16,
+    marginTop: 20
+  },
   image: {
-    width: 80,
-    height: 80,
-    marginBottom: 15,
+    width: 200,
+    height: 200,
     resizeMode: "contain"
   },
+  loginImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 5
+  },
+  buttonContainer: {
+    marginVertical: 16,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+  button: {
+    borderRadius: 5,
+    width: "100%",
+    padding: 10,
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderColor: "#ccc",
+  }
 })
