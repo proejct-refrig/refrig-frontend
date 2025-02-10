15ef37985f746f127012a4c792b9a569508a015b
Rowoon <dev.rowoon@gmail.com>
2025-02-10 17:24:04 +0900

    fix: expo 서버 에러로 인한 수정 사항 반영

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="main/index"/>
+      <Stack.Screen name="main/index" options={{ headerShown: false }} />
     </Stack>
   );
 }
