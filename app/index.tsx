b14fa5f74902e47d3446c38fa8d2195c9b9ec4bc
Rowoon <dev.rowoon@gmail.com>
2025-02-10 14:16:37 +0900

    feat: expo-image 설치 및 이미지 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from 'expo-image'
+
+const AppLogo = require('@/assets/images/refrig_logo.png')
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.loginBox}>
+        <Image source={AppLogo} style={styles.image}/>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5"
+  },
+  loginBox: {
+    width: 300,
+    height: 600,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+})
