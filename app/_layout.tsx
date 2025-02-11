40748bbfe4430ed8c9aa270f23e7c0a197d784c7
Rowoon <dev.rowoon@gmail.com>
2025-02-11 17:00:43 +0900

    feat: 커스텀 헤더 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,22 @@
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 import { store } from './store';
+import Header from '@/components/Header';
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="main/index" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+
+        {/* ✅ 메인 페이지 (커스텀 헤더 추가) */}
+        <Stack.Screen 
+          name="main/index" 
+          options={{ 
+            headerShown: true,
+            header: () => <Header />, // ✅ 커스텀 헤더 사용
+          }} 
+        />
       </Stack>
     </Provider>
   );
