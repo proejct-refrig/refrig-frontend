22cc3efda38c0bfec5ef9613b89e39dd92b8b285
Rowoon <dev.rowoon@gmail.com>
2025-02-13 13:08:43 +0900

    feat: 토큰 여부에 따라서 사용자 리디렉트

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,35 @@
-import { useRouter, useSegments } from 'expo-router'
-import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { checkLoginAndUpdateRedux } from '@/utils/auth';
 import { ActivityIndicator, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
+
 
 const IndexScreen: React.FC = () => {
   const router = useRouter();
-  const segments = useSegments();
-  const [isChecking, setIsChecking] = useState<boolean>(true);
+  const [isLoading, setisLoading] = useState<boolean>(true);
 
   useEffect(() => {
-  });
+    const checkAuth = async (): Promise<void> => {
+      try {
+        const token: string | null = await AsyncStorage.getItem("jwt_token");
 
-  if (isChecking) {
+        if (token) {
+          router.replace("/(tabs)"); // 로그인이 되어 있다면 메인페이지로 이동
+        } else {
+          router.replace("/login"); // 로그인이 풀렸거나 로그인 한적이 없으면 로그인 페이지로 이동
+        }
+      } catch (error) {
+        console.error('AsyncStorage Error!', error);
+        router.replace("/login");
+      } finally {
+        setisLoading(false); // 로그인이 정상적으로 되었다면 로딩 멈춤
+      }
+    }
+
+    checkAuth();
+  }, []);
+
+  if (isLoading) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" color="#0000ff" />
